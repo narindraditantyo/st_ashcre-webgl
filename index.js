a166fc54b26e3126c167b0f78f7b84a7134dce02
Gregorius Agung Narindra Aditantyo <narindra.aditantyo@gmail.com>
2020-12-16 19:52:29 +0700

    [DONE] 'ctrl' key to change camera and car movements | [WIP] Streetlamp switch and responsive window

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ import { OrbitControls } from './three.js/examples/jsm/controls/OrbitControls.js
 import { GLTFLoader } from './three.js/examples/jsm/loaders/GLTFLoader.js'
 
 let scene, camera, cameraFPV, cameraTPV, control, renderer
-let geometry, material, mesh, loader, texture, arrTexture
+let skybox, moonlight, asphalt, road, text1, text2, loadedCar
+let geometry, material, mesh, loader, texture, arrTexture, font
 let light
 
 let createMoonlight = () => {
-    light = new THREE.PointLight(0xF4F1C9, 5, 500, 1.5)
+    light = new THREE.PointLight(0xF4F1C9, 1, 1000, 1.5)
     light.position.set(0, 500, 250)
     light.shadow.mapSize.width = 2048
     light.shadow.mapSize.height = 2048
     light.castShadow = true
-    scene.add(light)
+    light.target = scene
+    return light
 }
 
 let createGround = (assetName, width, height, posX, posY, posZ, repX, repY) => {
@@ -28,9 +30,9 @@ let createGround = (assetName, width, height, posX, posY, posZ, repX, repY) => {
         map: texture,
     })
     mesh = new THREE.Mesh(geometry, material)
-    mesh.position.set(posX, posY, posZ)
     mesh.rotation.x = -(Math.PI/2)
-    scene.add(mesh)
+    mesh.position.set(posX, posY, posZ)
+    return mesh
 }
 
 let createBuilding = () => {
@@ -43,12 +45,14 @@ let createBuilding = () => {
         geometry = new THREE.BoxGeometry(25, buildingHeight*30, 25)
         loader = new THREE.TextureLoader()
         texture = loader.load('./assets/building.jpg')
+        texture.wrapS = THREE.RepeatWrapping
+        texture.wrapT = THREE.RepeatWrapping
+        texture.repeat.set(25/30, (buildingHeight*30)/30)
         material = new THREE.MeshStandardMaterial({
             roughness: 0.67,
             metalness: 0.65,
             map: texture
         })
-    
         mesh[i] = new THREE.Mesh(geometry, material)
         mesh[i].position.set(-40, 0, buildingPos)
         mesh[i].receiveShadow = true
@@ -69,7 +73,6 @@ let createBuilding = () => {
             metalness: 0.65,
             map: texture
         })
-    
         mesh[i] = new THREE.Mesh(geometry, material)
         mesh[i].position.set(40, 0, buildingPos)
         mesh[i].receiveShadow = true
@@ -79,25 +82,63 @@ let createBuilding = () => {
     }
 }
 
+let createHeadlightBulb = (posX) => {
+    geometry = new THREE.SphereGeometry(0.1)
+    material = new THREE.MeshPhongMaterial({
+        side: THREE.BackSide
+    })
+    mesh = new THREE.Mesh(geometry, material)
+    mesh.position.set(posX/5, 4/5, 13.5/5)
+    return mesh
+}
+
 let createHeadlight = (posX) => {
-    light = new THREE.SpotLight(0xFFFFFF, 1, 250, Math.PI/10)
-    light.position.set(posX, 0, -50)
+    light = new THREE.SpotLight(0xFFFFFF, 7.5, 250, THREE.Math.degToRad(7.75))
+    light.position.set(0, 0, -25/5)
+    light.castShadow = true
+
+    let targetObject = new THREE.Object3D()
+    targetObject.position.set(posX/5, 0, 100/5)
+    loadedCar.add(targetObject)
+
+    light.target = targetObject
     return light
 }
 
 let assembleCar = () => {
     loader = new GLTFLoader()
-    loader.load('./assets/model/model.glb', function(gltf) {
-        let loadedCar = gltf.scene
+    loader.load('./assets/model/model.glb', (gltf) => {
+        loadedCar = gltf.scene
         loadedCar.scale.set(5, 5, 5)
-        loadedCar.position.x = 7.5
+        loadedCar.position.set(7.5, 0, 100)
         loadedCar.rotation.y = Math.PI
 
-        let leftHeadlight = createHeadlight(4.5)
-        let rightHeadlight = createHeadlight(11.5)
+        let leftBulb = createHeadlightBulb(-3)
+        let leftHeadlight = createHeadlight(-3)
+        leftBulb.add(leftHeadlight)
+        loadedCar.add(leftBulb)
 
-        loadedCar.add(leftHeadlight)
-        loadedCar.add(rightHeadlight)
+        let rightBulb = createHeadlightBulb(3)
+        let rightHeadlight = createHeadlight(3)
+        rightBulb.add(rightHeadlight)
+        loadedCar.add(rightBulb)
+
+        window.addEventListener('keydown', (event) => {
+            var pressedKey = event.which
+
+            if(camera == cameraFPV) {
+                if(pressedKey == 87) {
+                    loadedCar.position.z -= 3
+                    cameraFPV.position.z -= 3
+                    camera = cameraFPV
+                } else if(pressedKey == 83) {
+                    loadedCar.position.z += 3
+                    cameraFPV.position.z += 3
+                    camera = cameraFPV
+                }
+            }
+        })
+
         scene.add(loadedCar)
     })
 }
@@ -127,7 +168,7 @@ let createContainer = (posX, posZ) => {
 }
 
 let createLid = (posX, posZ) => {
-    geometry = new THREE.CylinderGeometry(1, 2, 1, 4, 1)
+    geometry = new THREE.CylinderGeometry(0.5, 2, 1, 4, 1)
     material = new THREE.MeshPhongMaterial({
         wireframe: true
     })
@@ -137,8 +178,8 @@ let createLid = (posX, posZ) => {
     return mesh
 }
 
-let createBulb = (posX, posZ) => {
-    geometry = new THREE.SphereGeometry(1)
+let createBulb = (posX, posZ, side) => {
+    geometry = new THREE.SphereGeometry(0.5)
     material = new THREE.MeshPhongMaterial({
         side: THREE.BackSide
     })
@@ -147,51 +188,73 @@ let createBulb = (posX, posZ) => {
     return mesh
 }
 
-let createLight = (posX, posZ) => {
-    light = new THREE.PointLight(0xFFFFFF, 1, (250/6)*2, 2)
+let createLight = (intensity) => {
+    light = new THREE.PointLight(0xFFFFFF, 1, 175, 5)
     light.castShadow = true
     return light
 }
 
 let assembleStreetlamp = () => {
-    let assembledStreetlamp = []
-    let streetlampPos = (250/6)*3
+    let assembledStreetlamp = [[],[]]
+    let componentStreetlamp
+    let streetlampPos
+    let posX = -20
 
-    for(let i = 0; i < 4; i++) {
-        let pole = createPole(-20, streetlampPos)
-        let container = createContainer(-20, streetlampPos)
-        let lid = createLid(-20, streetlampPos)
-        let bulb = createBulb(-20, streetlampPos)
-        let streetLight = createLight(-20, streetlampPos)
+    for(let i = 0; i < 2; i++) {
+        streetlampPos = (250/6)*3
 
-        bulb.add(streetLight)
-        assembledStreetlamp[i] = [pole, container, lid, bulb]
+        for(let j = 0; j < 4; j++) {
+            componentStreetlamp = []
 
-        assembledStreetlamp[i].forEach(object => {
-            scene.add(object)
-        });
+            let pole = createPole(posX, streetlampPos)
+            let container = createContainer(posX, streetlampPos)
+            let lid = createLid(posX, streetlampPos)
+            let bulb = createBulb(posX, streetlampPos, THREE.BackSide)
+            let streetLight = createLight(1)
 
-        streetlampPos -= 500/6
+            bulb.add(streetLight)
+            componentStreetlamp.push(pole, container, lid, bulb)
+            assembledStreetlamp[i].push(componentStreetlamp)
+
+            assembledStreetlamp[i][j].forEach(object => {
+                scene.add(object)
+            })
+
+            window.addEventListener('click', (event) => {
+                if(event.target == assembledStreetlamp[i][j][3]) {
+                    if(assembledStreetlamp[i][j][3].getObjectByName('streetLights') == createLight(1)) {
+                        assembledStreetlamp[i][j][3] = createBulb(posX, streetlampPos, THREE.FrontSide)
+                        assembledStreetlamp[i][j][3].getObjectByName('streetLights') = createLight(0)
+                    } else {
+                        assembledStreetlamp[i][j][3] = createBulb(posX, streetlampPos, THREE.BackSide)
+                        assembledStreetlamp[i][j][3].getObjectByName('streetLights') = createLight(1)
+                    }
+                }
+            })
+
+            streetlampPos -= 500/6
+        }
+
+        posX += 40
     }
 
-    assembledStreetlamp = []
-    streetlampPos = 250/6*3
-    for(let i = 0; i < 4; i++) {
-        let pole = createPole(20, streetlampPos)
-        let container = createContainer(20, streetlampPos)
-        let lid = createLid(20, streetlampPos)
-        let bulb = createBulb(20, streetlampPos)
-        let streetLight = createLight(20, streetlampPos)
+    
+}
 
-        bulb.add(streetLight)
-        assembledStreetlamp[i] = [pole, container, lid, bulb]
-
-        assembledStreetlamp[i].forEach(object => {
-            scene.add(object)
-        });
-
-        streetlampPos -= 500/6
-    }
+let createText = (inputText, posY) => {
+    loader = new THREE.FontLoader()
+    loader.load('./three.js/examples/fonts/helvetiker_regular.typeface.json', (font) => {
+        geometry = new THREE.TextGeometry(inputText, {
+            font: font,
+            size: 6.25,
+            height: 3
+        })
+        geometry.center()
+        material = new THREE.MeshStandardMaterial()
+        mesh = new THREE.Mesh(geometry, material)
+        mesh.position.y = posY
+        scene.add(mesh)
+    })
 }
 
 let createSkybox = () => {
@@ -213,7 +276,7 @@ let createSkybox = () => {
     })
     mesh = new THREE.Mesh(geometry, material)
     mesh.position.set(0, -70, 0)
-    scene.add(mesh)
+    return mesh
 }
 
 let init = () => {
@@ -223,47 +286,57 @@ let init = () => {
     scene = new THREE.Scene()
 
     cameraTPV = new THREE.PerspectiveCamera(45, sWidth/sHeight, 0.1, 5000)
-    cameraTPV.position.set(0, 50, 325)
+    cameraTPV.position.set(-12.5, 25, 250)
     cameraTPV.lookAt(0, 0, 0)
 
-    cameraFPV = new THREE.PerspectiveCamera(45, sWidth/sHeight, 0.1, 500)
-    cameraFPV.position.set(7.5, 10, -25)
-    cameraFPV.lookAt(7.5, 10, -250)
+    cameraFPV = new THREE.PerspectiveCamera(45, sWidth/sHeight, 0.1, 5000)
+    cameraFPV.position.set(7.5, 7.5, 85)
+    cameraFPV.lookAt(7.5, 7.5, -250)
 
-    camera = cameraFPV
+    camera = cameraTPV
 
     renderer = new THREE.WebGLRenderer({
         antialias: true
-    });
+    })
     renderer.setSize(sWidth, sHeight)
     renderer.shadowMap.enabled = true
     renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
     document.body.appendChild(renderer.domElement)
 
-    window.addEventListener('resize', function() {
+    window.addEventListener('resize', () => {
         sWidth = window.innerWidth
         sHeight = window.innerHeight
         camera.aspect = sWidth/sHeight
         renderer.setSize(sWidth, sHeight)
     })
 
+    window.addEventListener('keydown', (event) => {
+        var pressedKey = event.which
+
+        if(pressedKey == 17) {
+            if(camera == cameraTPV) camera = cameraFPV
+            else camera = cameraTPV
+        }
+    })
+
     if(camera == cameraTPV) {
         control = new OrbitControls(camera, renderer.domElement)
         control.addEventListener('change', renderer)
-        control.maxDistance = 325
+        control.maxDistance = 250
+        control.maxPolarAngle = Math.PI/2.125
+        control.target.clamp
     }
 
     // Background
-    createSkybox()
+    skybox = createSkybox()
 
     // Ground
-    createGround('asphalt', 500, 500, 0, 0, 0, 20, 20)
-    createGround('road', 30, 500, 0, 0, 1, 1, 8)
-    
+    asphalt = createGround('asphalt', 500, 500, 0, 0, 0, 20, 20)
+    road = createGround('road', 30, 500, 0, 0.01, 0, 1, 8)
 
     // Lighting
-    createMoonlight()
+    moonlight = createMoonlight()
 
     // Building
     createBuilding()
@@ -271,8 +344,17 @@ let init = () => {
     // Streetlamps
     assembleStreetlamp()
 
+    // Text
+    createText('ST.', 11)
+    createText('ASHCRE', 4)
+
     // Car
-    // assembleCar()
+    assembleCar()
+
+    let objects = [skybox, moonlight, asphalt, road]
+    objects.forEach(object => {
+        scene.add(object)
+    })
 
     animate()
 }
